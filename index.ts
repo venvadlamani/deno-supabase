3dda1a633a8dd9c8d19d8b762d40dd3ab52b5b62
venvadlamani <122305594+venvadlamani@users.noreply.github.com>
2023-05-21 13:13:00 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { Pool } from 'https://deno.land/x/postgres@v0.17.0/mod.ts';
 
 // Get the connection string from the environment variable "DATABASE_URL"
 const databaseUrl = Deno.env.get('DATABASE_URL')!;
-
+//
 // Create a database pool with three connections that are lazily established
 const pool = new Pool(databaseUrl, 3, true);
 
